d1ef21e0b661bc74cbb2ad210cc5367e713e34af
KHALID185 <KHALID.1998.TAOUTI@GMAIL.COM>
2024-03-12 12:53:20 +0000

    first commit

---
+++ src/main/java/com/monguichet/MonGuichetApplication.java
@@ -0,0 +1,13 @@
+package com.monguichet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonGuichetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonGuichetApplication.class, args);
+    }
+
+}
